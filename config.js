f6bd3865ae08eb0825a8471b602675984a4628f9
Aayush Chhalerya <121503176+Aayush0909@users.noreply.github.com>
2024-03-28 14:01:32 +0530

    weather!

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "b6f4f74284d0dbcb281818cee8962c08"
+    }
+}
+
+module.exports = constants;
